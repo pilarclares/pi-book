85f97e7f10851a4e2531030f553c22593f53246f
Pilar Clares Cervantes <pilar.clarescervantes.practicas@telefonica.com>
2024-07-13 15:41:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'piBook',
+  webDir: 'dist'
+};
+
+export default config;
